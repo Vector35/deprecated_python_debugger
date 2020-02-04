6744cc5af6e8f30f36565f2bc29152c8e2839f8d
Andrew Lamoureux <andrew@vector35.com>
2020-02-04 14:44:43 -0500

    Reorganize tests, mach-o parse allow no entry_point_command

--- test.py
+++ test.py
@@ -27,18 +27,17 @@ adapter = None
 # UTILITIES
 #--------------------------------------------------------------------------
 
-def get_file_data(fpath):
+def parse_image(fpath):
 	with open(fpath, 'rb') as fp:
-		return fp.read()
+		data = fp.read()
 
-def get_entry(data):
 	# little endian macho
 	if data[0:4] == b'\xCF\xFA\xED\xFE':
 		assert data[4:8] == b'\x07\x00\x00\x01' # CPU_TYPE_X86_X64
 		ncmds = unpack('<I', data[16:20])[0]
 		#print('ncmds: %d' % ncmds)
 		vmaddr = None
-		entryoff = None
+		entryoff = 0
 		offs = 0x20
 		for i in range(ncmds):
 			cmd = unpack('<I', data[offs:offs+4])[0]
@@ -52,10 +51,10 @@ def get_entry(data):
 				#print('entryoff: %X' % entryoff)
 			offs += cmdsize
 		if not vmaddr:
-			raise Exception('couldn\'t locate segment_command_64 (where __TEXT loads)')
-		if entryoff == None:
-			raise Exception('couldn\'t locate entry_point_command (where main is)')
-		return vmaddr + entryoff
+			raise Exception('couldn\'t locate segment_command_64 (where __TEXT loads) in %s', fpath)
+		#if entryoff == None:
+		#	raise Exception('couldn\'t locate entry_point_command in macho (where main is)', fpath)
+		return (vmaddr, entryoff)
 
 	# PE
 	if data[0:2] == b'\x4d\x5a':
@@ -63,7 +62,7 @@ def get_entry(data):
 		assert data[e_lfanew:e_lfanew+6] == b'\x50\x45\x00\x00\x64\x86'
 		entryoff = unpack('<I', data[e_lfanew+0x28:e_lfanew+0x2C])[0]
 		vmaddr = unpack('<Q', data[e_lfanew+0x30:e_lfanew+0x38])[0]
-		return vmaddr + entryoff
+		return (vmaddr, entryoff)
 
 	# ELF
 	if data[0:4] == b'\x7FELF':
@@ -71,6 +70,7 @@ def get_entry(data):
 		assert data[5] == 1 # EI_DATA little endian
 		assert data[0x10:0x12] == b'\x02\x00' # e_type ET_EXEC
 		assert data[0x12:0x14] == b'\x3E\x00' # e_machine EM_X86_64
+		# TODO: separate ELF load address and offset
 		return unpack('<Q', data[0x18:0x20])[0]
 
 	raise Exception('unrecognized file type')
@@ -85,7 +85,11 @@ def get_entry(data):
 def test_prog_to_fpath(prog):
 	if platform.system() == 'Windows':
 		prog = prog + '.exe'
-	return os.path.join('testbins', prog)
+	tmp =  os.path.join('testbins', prog)
+	if '~' in tmp:
+		tmp = os.expanduser(tmp)
+	tmp = os.path.abspath(tmp)
+	return tmp
 
 def break_into(adapter):
 	print('sending break')
@@ -104,163 +108,192 @@ def assert_general_error(func):
 #------------------------------------------------------------------------------
 
 if __name__ == '__main__':
+	arg = sys.argv[1] if sys.argv[1:] else None
+
 	# one-off tests
-	if sys.argv[1:] and sys.argv[1] == 'oneoff':
-		fpath = test_prog_to_fpath('helloworld')
-		adapter = helpers.launch_get_adapter(fpath)
-		entry = get_entry(get_file_data(fpath))
-		print('entry: 0x%X' % entry)
-		adapter.breakpoint_set(entry)
-		adapter.go()
-		print('writing bad mem')
-		input()
-		adapter.mem_write(0, b'heheHAHAherherHARHAR')
-		adapter.quit()
+	if arg == 'oneoff':
+		fpath = test_prog_to_fpath('helloworld_func_pie')
+		print(fpath)
+		(load_addr, entry_offs) = parse_image(fpath)
+		print('load_addr: 0x%X' % load_addr)
+		print('entry_offs: 0x%X' % entry_offs)
 		sys.exit(0)
 
-	test_progs = ['helloworld', 'helloworld_thread', 'helloworld_loop']
-
-	#
-	# basic tests
-	#
-	#for prog in test_progs:
-	if False:
-		fpath = test_prog_to_fpath(prog)
-
-		data = get_file_data(fpath)
-		entry = get_entry(data)
-
-		print('file %s has entrypoint 0x%X' % (fpath, entry))
-		print('launching %s' % fpath)
-		adapter = helpers.launch_get_adapter(fpath)
-		print('rip: 0x%X' % adapter.reg_read('rip'))
-
-		# breakpoint set/clear should fail at 0
-		try:
-			adapter.breakpoint_clear(0)
-		except DebugAdapter.BreakpointClearError:
-			pass
-
-		try:
-			adapter.breakpoint_set(0)
-		except DebugAdapter.BreakpointSetError:
-			pass
-
-		# breakpoint set/clear should succeed at entrypoint
-		print('setting breakpoint at 0x%X' % entry)
-		adapter.breakpoint_set(entry)
-		print('clearing breakpoint at 0x%X' % entry)
-		adapter.breakpoint_clear(entry)
-		print('setting breakpoint at 0x%X' % entry)
-		adapter.breakpoint_set(entry)
-
-		# proceed to breakpoint
-		print('going')
-		(reason, info) = adapter.go()
-		assert reason == DebugAdapter.STOP_REASON.SIGNAL_TRAP
-		rip = adapter.reg_read('rip')
-		print('rip: 0x%X' % rip)
-		assert rip == entry
-
-		# single step
-		data = adapter.mem_read(rip, 15)
-		assert len(data) == 15
-		(asmstr, asmlen) = helpers.disasm1(data, 0)
-		adapter.breakpoint_clear(entry)
-		(reason, info) = adapter.step_into()
-		assert reason == DebugAdapter.STOP_REASON.SIGNAL_TRAP
-		rip2 = adapter.reg_read('rip')
-		print('rip2: 0x%X' % rip2)
-		assert rip + asmlen == rip2
-
-		# registers
-		for (ridx,rname) in enumerate(adapter.reg_list()):
-			width = adapter.reg_bits(rname)
-			#print('%d: %s (%d bits)' % (ridx, rname, width))
-		assert adapter.reg_bits('rax') == 64
-		assert adapter.reg_bits('rbx') == 64
-		assert_general_error(lambda: adapter.reg_bits('rzx'))
-
-		# reg read/write
-		rax = adapter.reg_read('rax')
-		rbx = adapter.reg_read('rbx')
-		assert_general_error(lambda: adapter.reg_read('rzx'))
-		adapter.reg_write('rax', 0xDEADBEEFAAAAAAAA)
-		assert adapter.reg_read('rax') == 0xDEADBEEFAAAAAAAA
-		adapter.reg_write('rbx', 0xCAFEBABEBBBBBBBB)
-		assert_general_error(lambda: adapter.reg_read('rzx'))
-		assert adapter.reg_read('rbx') == 0xCAFEBABEBBBBBBBB
-		adapter.reg_write('rax', rax)
-		assert adapter.reg_read('rax') == rax
-		adapter.reg_write('rbx', rbx)
-		assert adapter.reg_read('rbx') == rbx
-
-		# mem read/write
-		addr = adapter.reg_read('rip')
-		data = adapter.mem_read(addr, 256)
-		assert_general_error(lambda: adapter.mem_write(0, b'heheHAHAherherHARHAR'))
-		data2 = b'\xAA' * 256
-		adapter.mem_write(addr, data2)
-		assert_general_error(lambda: adapter.mem_read(0, 256))
-		assert adapter.mem_read(addr, 256) == data2
-		adapter.mem_write(addr, data)
-		assert adapter.mem_read(addr, 256) == data
-
-		print('quiting')
-		adapter.quit()
-		adapter = None
-
-	#
-	# thread test
-	#
-	fpath = test_prog_to_fpath('helloworld_thread')
-	adapter = helpers.launch_get_adapter(fpath)
-	print('scheduling break in .5 seconds')
-	threading.Timer(.5, break_into, [adapter]).start()
-	print('going')
-	adapter.go()
-	print('back')
-	print('switching to bad thread')
-	assert_general_error(lambda: adapter.thread_select(999))
-	print('asking for threads')
-	if platform.system() == 'Windows':
-		# main thread at WaitForMultipleObjects() + 4 created threads + debugger thread
-		nthreads_expected = 6
+	tests = []
+	if arg in ['asm', 'assembly', 'assembler']:
+		tests = ['assembly']
+	elif arg in ['thread', 'threads', 'threading']:
+		tests = ['thread']
+	elif arg in ['basic']:
+		tests = ['basic']
 	else:
-		# main thread at pthread_join() + 4 created threads
-		nthreads_expected = 5
-	tids = adapter.thread_list()
-	assert len(tids) == nthreads_expected
-	tid_active = adapter.thread_selected()
-	rips = []
-	for tid in tids:
-		adapter.thread_select(tid)
-		rip = adapter.reg_read('rip')
-		rips.append(rip)
-		seltxt = '<--' if tid == tid_active else ''
-		print('thread %02d: rip=0x%016X %s' % (tid, rip, seltxt))
-	assert rips[0] != rips[1] # thread at WaitForMultipleObjects()/pthread_join() should be different
-	print('switching to bad thread')
-	assert_general_error(lambda: adapter.thread_select(999))
-	secs = .5
-	print('scheduling break in %d second(s)' % secs)
-	threading.Timer(secs, break_into, [adapter]).start()
-	print('going')
-	adapter.go()
-	print('back')
-	print('checking for %d threads' % nthreads_expected)
-	assert len(adapter.thread_list()) == nthreads_expected
-	# ensure the rip's are in different locations (that the continue actually continued)
-	rips2 = []
-	for tid in tids:
-		adapter.thread_select(tid)
-		rip = adapter.reg_read('rip')
-		rips2.append(rip)
-	print('checking that at least one thread progressed')
-	print(' rips: ', rips)
-	print('rips2: ', rips2)
-	assert list(filter(lambda x: not x, [rips[i]==rips2[i] for i in range(len(rips))])) != []
-	print('done')
-	adapter.quit()
+		tests = ['assembly', 'thread', 'basic']
+
+	if 'basic' in tests:
+		for prog in ['helloworld', 'helloworld_loop', 'helloworld_thread', 'helloworld_func',
+			'helloworld_pie', 'helloworld_thread_pie', 'helloworld_loop_pie', 'helloworld_func_pie',
+			]:
+
+			fpath = test_prog_to_fpath(prog)
+
+			print('----------------------------------------------------------------')
+			print('BASIC test on %s' % fpath)
+			print('----------------------------------------------------------------')
+
+			(load_addr, entry_offs) = parse_image(fpath)
+			entry = load_addr + entry_offs
+			print('(file) load addr: 0x%X' % load_addr)
+			print('(file) entry offset: 0x%X' % entry_offs)
+
+			print('launching')
+			adapter = helpers.launch_get_adapter(fpath)
+
+			# learn load address, entrypoint
+			#
+			module2addr = adapter.mem_modules()
+			print(module2addr)
+			print(fpath)
+			assert fpath in module2addr
+			if not '_pie' in prog:
+				print('non-pie module should hold file\'s specified load and entry')
+				assert module2addr[fpath] == load_addr
+			else:
+				load_addr = module2addr[fpath]
+				print('pie module, file load 0x%X overridden with 0x%X, new entry 0x%X' %
+					(load_addr, module2addr[fpath], module2addr[fpath]+entry_offs))
+				entry = load_addr + entry_offs
+
+			print('rip: 0x%X' % adapter.reg_read('rip'))
+
+			# breakpoint set/clear should fail at 0
+			try:
+				adapter.breakpoint_clear(0)
+			except DebugAdapter.BreakpointClearError:
+				pass
+
+			try:
+				adapter.breakpoint_set(0)
+			except DebugAdapter.BreakpointSetError:
+				pass
+
+			# breakpoint set/clear should succeed at entrypoint
+			print('setting breakpoint at 0x%X' % entry)
+			adapter.breakpoint_set(entry)
+			print('clearing breakpoint at 0x%X' % entry)
+			adapter.breakpoint_clear(entry)
+			print('setting breakpoint at 0x%X' % entry)
+			adapter.breakpoint_set(entry)
+
+			# proceed to breakpoint
+			print('going')
+			(reason, info) = adapter.go()
+			assert reason == DebugAdapter.STOP_REASON.SIGNAL_TRAP
+			rip = adapter.reg_read('rip')
+			print('rip: 0x%X' % rip)
+			assert rip == entry
+
+			# single step
+			data = adapter.mem_read(rip, 15)
+			assert len(data) == 15
+			(asmstr, asmlen) = helpers.disasm1(data, 0)
+			adapter.breakpoint_clear(entry)
+			(reason, info) = adapter.step_into()
+			assert reason == DebugAdapter.STOP_REASON.SIGNAL_TRAP
+			rip2 = adapter.reg_read('rip')
+			print('rip2: 0x%X' % rip2)
+			assert rip + asmlen == rip2
+
+			print('registers')
+			for (ridx,rname) in enumerate(adapter.reg_list()):
+				width = adapter.reg_bits(rname)
+				#print('%d: %s (%d bits)' % (ridx, rname, width))
+			assert adapter.reg_bits('rax') == 64
+			assert adapter.reg_bits('rbx') == 64
+			assert_general_error(lambda: adapter.reg_bits('rzx'))
+
+			print('registers read/write')
+			rax = adapter.reg_read('rax')
+			rbx = adapter.reg_read('rbx')
+			assert_general_error(lambda: adapter.reg_read('rzx'))
+			adapter.reg_write('rax', 0xDEADBEEFAAAAAAAA)
+			assert adapter.reg_read('rax') == 0xDEADBEEFAAAAAAAA
+			adapter.reg_write('rbx', 0xCAFEBABEBBBBBBBB)
+			assert_general_error(lambda: adapter.reg_read('rzx'))
+			assert adapter.reg_read('rbx') == 0xCAFEBABEBBBBBBBB
+			adapter.reg_write('rax', rax)
+			assert adapter.reg_read('rax') == rax
+			adapter.reg_write('rbx', rbx)
+			assert adapter.reg_read('rbx') == rbx
+
+			print('mem read/write')
+			addr = adapter.reg_read('rip')
+			data = adapter.mem_read(addr, 256)
+			assert_general_error(lambda: adapter.mem_write(0, b'heheHAHAherherHARHAR'))
+			data2 = b'\xAA' * 256
+			adapter.mem_write(addr, data2)
+			assert_general_error(lambda: adapter.mem_read(0, 256))
+			assert adapter.mem_read(addr, 256) == data2
+			adapter.mem_write(addr, data)
+			assert adapter.mem_read(addr, 256) == data
+
+			print('quiting')
+			adapter.quit()
+			adapter = None
+
+	if 'thread' in tests:
+		fpath = test_prog_to_fpath('helloworld_thread')
+
+		print('----------------------------------------------------------------')
+		print('BASIC test on %s' % fpath)
+		print('----------------------------------------------------------------')
+
+		adapter = helpers.launch_get_adapter(fpath)
+		print('scheduling break in .5 seconds')
+		threading.Timer(.5, break_into, [adapter]).start()
+		print('going')
+		adapter.go()
+		print('back')
+		print('switching to bad thread')
+		assert_general_error(lambda: adapter.thread_select(999))
+		print('asking for threads')
+		if platform.system() == 'Windows':
+			# main thread at WaitForMultipleObjects() + 4 created threads + debugger thread
+			nthreads_expected = 6
+		else:
+			# main thread at pthread_join() + 4 created threads
+			nthreads_expected = 5
+		tids = adapter.thread_list()
+		assert len(tids) == nthreads_expected
+		tid_active = adapter.thread_selected()
+		rips = []
+		for tid in tids:
+			adapter.thread_select(tid)
+			rip = adapter.reg_read('rip')
+			rips.append(rip)
+			seltxt = '<--' if tid == tid_active else ''
+			print('thread %02d: rip=0x%016X %s' % (tid, rip, seltxt))
+		assert rips[0] != rips[1] # thread at WaitForMultipleObjects()/pthread_join() should be different
+		print('switching to bad thread')
+		assert_general_error(lambda: adapter.thread_select(999))
+		secs = .5
+		print('scheduling break in %d second(s)' % secs)
+		threading.Timer(secs, break_into, [adapter]).start()
+		print('going')
+		adapter.go()
+		print('back')
+		print('checking for %d threads' % nthreads_expected)
+		assert len(adapter.thread_list()) == nthreads_expected
+		# ensure the rip's are in different locations (that the continue actually continued)
+		rips2 = []
+		for tid in tids:
+			adapter.thread_select(tid)
+			rip = adapter.reg_read('rip')
+			rips2.append(rip)
+		print('checking that at least one thread progressed')
+		#print(' rips: ', rips)
+		#print('rips2: ', rips2)
+		assert list(filter(lambda x: not x, [rips[i]==rips2[i] for i in range(len(rips))])) != []
+		print('done')
+		adapter.quit()
 
 	print('TESTS PASSED!')
