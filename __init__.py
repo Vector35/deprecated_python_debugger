4b7433a99a944ebf77618a927d6cf4e4672dac8c
Andrew Lamoureux <andrew@vector35.com>
2020-04-10 14:39:42 -0400

    Runtime check minimum Binja version

--- __init__.py
+++ __init__.py
@@ -2,6 +2,14 @@
 # Start scripts with the following:
 # import debugger
 # dbg = debugger.get(bv)
+
+# warn if minimum version not met
+import binaryninja
+min_version = 2085
+(major, minor, incrementing) = map(int, binaryninja.core_version().split('-')[0].split('.'))
+if incrementing < min_version:
+	binaryninja.interaction.show_message_box("Debugger Version Check Failed", "Debugger relies on features and fixes present in Binary Ninja >= %d. Errors may follow, please update." % min_version)
+
 from . import binjaplug
 
 """
