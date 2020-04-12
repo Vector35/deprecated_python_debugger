64f85b2bea3bc9909edb02f9460721b3004122f6
Glenn Smith <glenn@vector35.com>
2020-04-12 04:38:19 -0400

    Fix version warning for headless and development builds

--- __init__.py
+++ __init__.py
@@ -5,10 +5,16 @@
 
 # warn if minimum version not met
 import binaryninja
+import sys
 min_version = 2085
-(major, minor, incrementing) = map(int, binaryninja.core_version().split('-')[0].split('.'))
-if incrementing < min_version:
-	binaryninja.interaction.show_message_box("Debugger Version Check Failed", "Debugger relies on features and fixes present in Binary Ninja >= %d. Errors may follow, please update." % min_version)
+# git builds end with ' development'
+if not binaryninja.core_version().endswith(" development"):
+	(major, minor, incrementing) = map(int, binaryninja.core_version().split('-')[0].split('.'))
+	if incrementing < min_version:
+		if binaryninja.core_ui_enabled():
+			binaryninja.interaction.show_message_box("Debugger Version Check Failed", "Debugger relies on features and fixes present in Binary Ninja >= %d. Errors may follow, please update." % min_version)
+		else:
+			print("Debugger relies on features and fixes present in Binary Ninja >= %d. Errors may follow, please update." % min_version, file=sys.stderr)
 
 from . import binjaplug
 
