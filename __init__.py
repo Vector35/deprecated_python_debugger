24578e3d18385e7e3f9a9bde46d8467830310acd
Andrew Lamoureux <andrew@vector35.com>
2020-04-13 13:42:11 -0400

    Read plugin.json for version check, close #77

--- __init__.py
+++ __init__.py
@@ -3,23 +3,32 @@
 # import debugger
 # dbg = debugger.get(bv)
 
-# warn if minimum version not met
-import binaryninja
-import sys
+import os
 import re
+import sys
+import binaryninja
 
-min_version = 2085
-version = binaryninja.core_version()
-incrementing = re.match(r'^\d+\.\d+\.(\d+)', version).group(1)
+# warn if minimum version not met
+try:
+	import json
+	fpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'plugin.json')
+	with open(fpath) as fp:
+	    data = json.load(fp)
+	    min_version = data['minimumbinaryninjaversion']
 
-# git builds end with ' development'
-if not version.endswith('development'):
-	if int(incrementing) < min_version:
-		message = "Debugger relies on features and fixes present in Binary Ninja >= {}. Errors may follow, please update.".format(min_version)
-		if binaryninja.core_ui_enabled():
-			binaryninja.interaction.show_message_box("Debugger Version Check Failed", message)
-		else:
-			print(message, file=sys.stderr)
+	version = binaryninja.core_version()
+	incrementing = re.match(r'^\d+\.\d+\.(\d+)', version).group(1)
+
+	# git builds end with ' development'
+	if not version.endswith('development'):
+		if int(incrementing) < int(min_version):
+			message = "Debugger relies on features and fixes present in Binary Ninja >= {}. Errors may follow, please update.".format(min_version)
+			if binaryninja.core_ui_enabled():
+				binaryninja.interaction.show_message_box("Debugger Version Check Failed", message)
+			else:
+				print(message, file=sys.stderr)
+except:
+	pass
 
 from . import binjaplug
 
